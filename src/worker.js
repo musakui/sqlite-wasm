d836cfb3720dda529adfde3500429e3a11eeb2a2
randName <kwokshungit@gmail.com>
2023-12-28 20:57:03 +0800

    extract capi

--- src/worker.js
+++ src/worker.js
@@ -1,17 +1,16 @@
 import { init } from '../lib/index.js'
+import { sqlite3_js_db_export } from '../lib/capi_extras_with_export.js'
 import { runDemo } from './demo.js'
-
-const wasmSrc = new URL('../sqlite-wasm/jswasm/sqlite3.wasm', import.meta.url)
 
 const DB_FILE = '/db.sqlite3'
 
-const sqlite = await init(fetch(wasmSrc))
+const sqlite = await init()
 const PoolUtil = await sqlite.installOpfsSAHPoolVfs()
 const db = new PoolUtil.OpfsSAHPoolDb(DB_FILE)
 
 const utils = {
 	log: (m, b) => self.postMessage({ message: m, body: b }),
-	exportDb: () => sqlite.capi.sqlite3_js_db_export(db).buffer,
+	exportDb: () => sqlite3_js_db_export(db).buffer,
 	importDb: (p) => {
 		if (!(p instanceof ArrayBuffer)) return 0
 		PoolUtil.importDb(DB_FILE, p)
